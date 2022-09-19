badd0fc937c51558c6ba2ca2295314ab739fc4c8
Eren Düzova <erenduzova@gmail.com>
2022-09-19 12:44:35 +0300

    Create TODO list

---
+++ main.py
@@ -0,0 +1,11 @@
+STOCK = "TSLA"
+COMPANY_NAME = "Tesla Inc"
+
+# TODO 1: Use https://www.alphavantage.co
+# When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
+
+# TODO 2: Use https://newsapi.org
+# Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME.
+
+# TODO 3: Use https://www.twilio.com
+# Send a seperate message with the percentage change and each article's title and description to your phone number.

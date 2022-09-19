0854fff6afc60cdfd0d4a67be2402b8863fcfb0d
Eren Düzova <erenduzova@gmail.com>
2022-09-19 13:41:49 +0300

    Set Environment Variables
    Install python-dotenv, create ".env" file.
    For security and privacy use ".env" file for keys and secret information.

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
 STOCK = "TSLA"
 COMPANY_NAME = "Tesla Inc"
 
 # TODO 1: Use https://www.alphavantage.co
 # When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
+API_KEY_ALPVAN = os.getenv("API_KEY_ALPHAVANTAGE")
+
 
 # TODO 2: Use https://newsapi.org
 # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME.
+
+
 
 # TODO 3: Use https://www.twilio.com
 # Send a seperate message with the percentage change and each article's title and description to your phone number.

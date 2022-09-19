50e03c3c9a5852acb3eadfbc74e1ac7e3a12d40c
Eren Düzova <erenduzova@gmail.com>
2022-09-19 14:57:11 +0300

    Get Stock Data
    Get the last two day's closing data for comparison.

--- main.py
+++ main.py
@@ -1,15 +1,44 @@
 from dotenv import load_dotenv
 import os
+import requests
 
 load_dotenv()
+
+# API KEYS
+API_KEY_STOCK = os.getenv("API_KEY_ALPHAVANTAGE")
 
 STOCK = "TSLA"
 COMPANY_NAME = "Tesla Inc"
 
+# Stock parameters
+URL_STOCK = "https://www.alphavantage.co/query"
+ALPHAVANTAGE_PARAMETERS = {
+    "function": "TIME_SERIES_DAILY",
+    "symbol": STOCK,
+    "apikey": API_KEY_STOCK
+}
+
+
+def get_last_two_stock_close():
+    """
+    This function gets the last two day's closing data.
+    Returns a tuple of this data.
+    :return: last_close, before_last_close
+    """
+    response = requests.get(url=URL_STOCK, params=ALPHAVANTAGE_PARAMETERS)
+    response.raise_for_status()
+    stock_data = response.json()["Time Series (Daily)"]
+    last_two_days_data = list(stock_data.values())[:2]
+    last_close = float(last_two_days_data[0]["4. close"])
+    before_last_close = float(last_two_days_data[1]["4. close"])
+    return last_close, before_last_close
+
+
 # TODO 1: Use https://www.alphavantage.co
 # When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
-API_KEY_ALPVAN = os.getenv("API_KEY_ALPHAVANTAGE")
 
+last_closes = get_last_two_stock_close()
+print(last_closes)
 
 # TODO 2: Use https://newsapi.org
 # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME.

6f09ff4116550d96d31ea4ea3137099cfccb4963
Eren Düzova <erenduzova@gmail.com>
2022-09-20 21:59:04 +0300

    Find Difference (%)
    Compare last two stock close data and find the difference in percentage.

--- main.py
+++ main.py
@@ -34,12 +34,19 @@ def get_last_two_stock_close():
     return last_close, before_last_close
 
 
+def compare_closes(closes):
+    diff = closes[0] - closes[1]
+    diff_per = (diff / (closes[1] / 100))
+
+    return float("{:.2f}".format(diff_per))
+
+
 # TODO 1: Use https://www.alphavantage.co
 # When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
 
 last_closes = get_last_two_stock_close()
-print(last_closes)
-
+compare = compare_closes(closes=last_closes)
+print(compare)
 # TODO 2: Use https://newsapi.org
 # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME.
 
